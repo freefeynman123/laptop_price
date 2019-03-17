afdd1947482009eaaba19b7b532469cd60d2dd5e
Lukasz <lukasz3.bala@gmail.com>
2019-03-17 15:37:38 +0100

    Adding cat features

--- visuals.py
+++ visuals.py
@@ -1,11 +1,17 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.datasets import load_digits
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
+from typing import Union
+import seaborn as sns
 
+"""
+Taken from scikit-learn repository
+"""
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
                         n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
@@ -88,6 +94,59 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
 
     plt.legend(loc="best")
     return plt
+
+"""
+This function was taken from 
+
+https://github.com/mrtovsky/Paralytics/tree/master/paralytics
+
+repository
+"""
+
+def multipleplots(data, x, nrows=1, ncols=1, figsize=(8, 6), 
+                  plot_type='scatterplot', **params):
+    """Paints any specified plot from the seaborn library multiple times.
+
+    Parameters
+    ----------
+    data: DataFrame
+        Data which columns will be plotted.
+
+    x: list
+        List of columns to plot one after another.
+
+    n{rows, cols}: int (default: 1)
+        Number of {rows, cols} transferred to plt.subplots().
+    
+    figsize: list-like
+        Size of every figure created on subsequent axes.
+
+    plot_type: string
+        Specific plot from the seaborn library to be plotted multiple times
+        against every given element from x.
+
+    params: dict
+        Dictionary of optional parameters passed to the chosen seaborn plot.
+
+    Returns
+    -------
+    None
+    
+    """
+    assert isinstance(data, pd.DataFrame), \
+        'Input "data" must be an instance of pandas.DataFrame()!'
+    assert isinstance(x, list), \
+        'Input "x" must be the list of data columns to plot!'
+    
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    axes = axes.flatten()
+    for idx, ax in enumerate(axes):
+        plt.sca(ax)
+        try:
+            getattr(sns, plot_type)(x[idx], data=data, **params)
+        except AttributeError as e:
+            print('Consider changing the "plot_type" parameter.')
+            raise
 
 
 
